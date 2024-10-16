39a2ddf040bbd237703f6c29f10381a5819eec31
elisagc <elisagcubero@gmail.com>
2024-10-16 13:37:35 +0200

    feat: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Header.module.css';
+import Loader from './common/Loader';
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className={classes['app-header']}>
-      <span>Podcaster</span>
-      <span>Loading...</span>
+      <Link to={'/'}>Podcaster</Link>
+      <Loader />
     </div>
   );
 };
