e36bc7c6fcce371a1051aa2dc1da31e1bb4e76c5
elisagc <elisagcubero@gmail.com>
2024-10-17 15:05:18 +0200

    feat: create layout components folder

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import classes from './Header.module.css';
-import Loader from './common/Loader';
+import Loader from '../common/Loader';
 import { Link } from 'react-router-dom';
+import { useLoading } from '../../context/LoadingContext';
 const Header = () => {
+  const { loading } = useLoading();
+
   return (
     <div className={classes['app-header']}>
       <Link to={'/'}>Podcaster</Link>
-      <Loader />
+      {loading && <Loader />}
     </div>
   );
 };
