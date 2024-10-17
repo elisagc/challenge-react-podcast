d66c805439ac23b4462b6872f7040825782fc990
elisagc <elisagcubero@gmail.com>
2024-10-17 15:09:33 +0200

    feat: create loading context

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import EpisodePage from './pages/EpisodePage';
 import HomePage from './pages/HomePage';
 import PodcastPage from './pages/PodcastPage';
-import Layout from './components/Layout';
+import Layout from './components/layout/Layout';
+import { LoadingProvider } from './context/LoadingContext';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/podcast/:podcastId" element={<PodcastPage />} />
-          <Route
-            path="/podcast/:podcasId/episode/:episodeId"
-            element={<EpisodePage />}
-          />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
+      <LoadingProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/podcast/:podcastId" element={<PodcastPage />} />
+            <Route
+              path="/podcast/:podcastId/episode/:episodeId"
+              element={<EpisodePage />}
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </LoadingProvider>
     </div>
   );
 }
