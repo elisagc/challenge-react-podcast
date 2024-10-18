c4ff132b80cebd0772ba2382f58a51c828421550
elisagc <elisagcubero@gmail.com>
2024-10-18 11:44:06 +0200

    feat: sanitize episode description

--- src/components/episode/EpisodeCard.tsx
+++ src/components/episode/EpisodeCard.tsx
@@ -1,15 +1,23 @@
 import { PodcastEpisode } from '../../interfaces/podcast';
 import Player from '../common/Player';
 import classes from './EpisodeCard.module.css';
-
+import DOMPurify from 'dompurify';
 interface Props {
   episode: PodcastEpisode;
 }
+
 const EpisodeCard = ({ episode }: Props) => {
+  const sanitizedDescription = DOMPurify.sanitize(
+    episode.episodeDescription ?? ''
+  );
   return (
     <div className={classes.container}>
       <h2 className={classes.title}>{episode.title}</h2>
-      <p className={classes.description}>{episode.episodeDescription}</p>
+      <div
+        className={classes.description}
+        dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
+      />
+
       {episode.episodeTrackUrl && <Player trackUrl={episode.episodeTrackUrl} />}
     </div>
   );
